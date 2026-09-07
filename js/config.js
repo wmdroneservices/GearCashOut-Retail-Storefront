519ef3d67cdbf5a13580017cbf7ad51987cd9845
west-midlands-drone-services <leannelaurenlowe@hotmail.com>
2026-09-07 20:55:37 +0100

    Stage 1 retail storefront foundation: js/config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = "https://npdpopaoazbpmwsgyosp.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Plc9kcyye1asKxTJOmGdhQ_dP_LX59o";
+export const STOREFRONT_KEY = "retail";
+export const PAGE_SIZE = 120;
